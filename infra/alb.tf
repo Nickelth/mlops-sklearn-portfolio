96b0af94b24d7885d0b2859d65a08024ce0349c0
Optiplex <Nickelth.nike@gmail.com>
2025-09-14 08:18:02 +0900

    infra: terraform ALB + Network

---
+++ infra/alb.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "api" {
+  name               = "mlops-api-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+  enable_deletion_protection = false
+  tags = { Project = "mlops-sklearn-portfolio" }
+}
+
+resource "aws_lb_target_group" "api" {
+  name        = "mlops-api-tg"
+  port        = 8000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/health"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
+  tags = { Project = "mlops-sklearn-portfolio" }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
