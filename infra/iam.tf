e59b433a04f048a84f3c481dc28d559df287298d
Optiplex <Nickelth.nike@gmail.com>
2025-09-14 15:59:43 +0900

    infra: 文法校正

--- infra/iam.tf
+++ infra/iam.tf
@@ -2,7 +2,7 @@ data "aws_iam_policy_document" "ecs_task_assume" {
   statement {
     actions = ["sts:AssumeRole"]
     principals { 
-        type = "Service", 
+        type = "Service"
         identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
