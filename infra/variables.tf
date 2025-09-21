04436b957292956a823ab8b3126223616a6b3ac5
Optiplex <Nickelth.nike@gmail.com>
2025-09-21 16:31:36 +0900

    Makefile: 定義名重複削除

--- infra/variables.tf
+++ infra/variables.tf
@@ -42,9 +42,3 @@ variable "ecr_repository_url" {
   default     = ""
   description = "ECR repository URI, e.g. 123456789012.dkr.ecr.us-west-2.amazonaws.com/mlops-sklearn-portfolio"
 }
-
-variable "bucket_name" {
-  type        = string
-  default     = ""
-  description = "ECR repository URI, e.g. 123456789012.dkr.ecr.us-west-2.amazonaws.com/mlops-sklearn-portfolio"
-}
