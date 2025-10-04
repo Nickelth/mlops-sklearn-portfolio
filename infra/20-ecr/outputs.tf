d74a27e7eea1cc398cddb22a60986b0b150e5acd
Optiplex <Nickelth.nike@gmail.com>
2025-10-04 16:37:13 +0900

    infra: Error: Reference to undeclared module

--- infra/20-ecr/outputs.tf
+++ infra/20-ecr/outputs.tf
@@ -7,9 +7,3 @@ output "bucket_arn" {
   value       = try(aws_s3_bucket.artifacts.arn, null)
   description = "Artifacts bucket ARN (null when bucket is not planned)"
 }
-
-output "alb_dns"               { value = aws_lb.api.dns_name }
-output "tg_arn"                { value = aws_lb_target_group.api.arn }
-output "tasks_security_group_id" { value = aws_security_group.tasks.id }
-output "public_subnet_ids"     { value = [ for s in aws_subnet.public : s.id ] }
-output "log_group_name"        { value = aws_cloudwatch_log_group.api.name }
